24d7b84ecc7740a4465550a5d7b6d6b112db6f80
zs0308 <835421822@qq.com>
2018-09-17 17:48:11 +0800

    from  as test

--- app/src/main/java/com/zs/superdemo/leco/utils/MLog.java
+++ app/src/main/java/com/zs/superdemo/leco/utils/MLog.java
@@ -4,24 +4,38 @@ import android.util.Log;
 
 public class MLog {
 
-	private static boolean DEBUG = true;
-	private static String TAG = "hua";
+    private static boolean DEBUG = true;
+    private static String TAG = "hua";
 
-	public static void i(String s) {
-		if (!DEBUG)
-			return;
-		Log.i(TAG, s);
-	}
+    public static void i(String s) {
+        if (!DEBUG)
+            return;
+        Log.i(TAG, s);
+    }
 
-	public static void d(String s) {
-		if (!DEBUG)
-			return;
-		Log.d(TAG, s);
-	}
+    public static void d(String s) {
+        if (!DEBUG)
+            return;
+        Log.d(TAG, s);
+    }
 
-	public static void e(String s) {
-		if (!DEBUG)
-			return;
-		Log.e(TAG, s);
-	}
+    public static void e(String s) {
+        if (!DEBUG)
+            return;
+        e(TAG, s);
+    }
+
+    //打印超长log
+    public static void e(String tag, String msg) {  //信息太长,分段打印
+        //因为String的length是字符数量不是字节数量所以为了防止中文字符过多，
+        //  把4*1024的MAX字节打印长度改为2001字符数
+        int max_str_length = 2001 - tag.length();
+        //大于4000时
+        while (msg.length() > max_str_length) {
+            Log.e(tag, msg.substring(0, max_str_length));
+            msg = msg.substring(max_str_length);
+        }
+        //剩余部分
+        Log.e(tag, msg);
+    }
 }
